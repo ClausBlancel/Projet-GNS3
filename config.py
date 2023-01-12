18c207b217bf7d44161d100bd3e38e856993bd4a
Lucas <lucas.leblanc@orange.fr>
2023-01-12 18:14:24 +0100

    Setup tests + get des protocoles

--- config.py
+++ config.py
@@ -7,12 +7,19 @@ def initXML():
     return tree.getroot()
 
 def getRouterName(root, nb):
-    return root[0][nb].get('name')
+    return root[0][nb].get('hostname')
 
-def getInterfaceAddress(root, router):
-    return root[0][router]
+def getInterfaceAddress(root, router, nbInterface):
+    return root[0][router][nbInterface][2].text
+
+def getRoutersInAS(root, ASnumber):
+    lRouters = []
+    for router in root[1][ASnumber].iter('contains'):
+        lRouters.append(router.text)
+    return lRouters
+
+def getASProtocol(root, ASnumber):
+    return root.find('./AutonomousSystems/AS[@name="' + ASnumber + '"]/protocol').text
 
 if __name__ == "__main__":
     root = initXML()
-    print(getRouterName(root, 1))
-    print(getInterfaceAddress(root, 1))
