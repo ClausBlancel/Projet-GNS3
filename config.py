2cf6c594acc9833eab5bd757016ab522d5cec552
Adam Laalouj <adam.laalouj@insa-lyon.fr>
2023-01-19 06:21:43 +0100

    modification routeurs.xml et le code python pour mieux génerer les fichiers avec notamment ebgp qu'il reste à finir en corrigeant les adresses remote-as et d'autres détails

--- config.py
+++ config.py
@@ -1,29 +1,75 @@
 import xml.etree.ElementTree as ET
 
 # fichier xml qui regroupe 
-configfile = '/home/alaalouj/Documents/3TC/GNS3/automatisation/source/routeurs.xml' #comment automatiser ça?
+configfile = './routeurs.xml'
 
 # definir les fonctions dont on aura besoin dans le main
 def initXML():
     tree = ET.parse(configfile) # transforme le fichier en ElementTree
     return tree.getroot() # selectionne la racine de l'arbre à elements (l'ET)
 
-def getRouterName(root, nb):
-    return root[0][nb].get('hostname')
+def getRouterName(router): #A jour
+    return router.get('hostname')
 
-def getInterfaceAddress(root, router, nbInterface):
-    return root[0][router][nbInterface][2].text
+def getRouterNumber(router) : #A jour
+    return router.get('Rnumber')
 
-def getRoutersInAS(root, ASnumber):
+def getNeighbors(router) : #A jour
+    lNeighbors = []
+    for i in router :
+        lNeighbors.append(i.find('neighbor').text)
+    return lNeighbors
+
+def getInterfaceAddress(interface):
+    return interface.find('address')
+
+def getInterfaceNei(interface):
+    return interface.find('neighbor').text
+
+def getInterfaceFromNei(router, nei):
+    res,n = (0, 0)
+    for i in router :
+        voisin = getInterfaceNei(i)
+        if voisin == nei :
+            res = n
+        n+=1
+    print(res)
+    return router[res]
+
+def getASList(root):
+    lAS = []
+    for AS in root.iter('AS'):
+        lAS.append(AS)
+    return lAS
+
+def getASPosition(root, ASnumber) : #A jour
+    for i in range(len(root)) :
+        name = root[i].get('name')
+        if ASnumber == int(name) : return i  
+
+def getRoutersInAS(root, ASnumber): # A jour
     lRouters = []
-    for router in root[1][ASnumber].iter('contains'):
-        lRouters.append(router.text)
+    n = getASPosition(root, ASnumber)
+    for router in root[n].iter('router'):
+        lRouters.append(str(router.get('hostname')))
     return lRouters
 
-def getASProtocol(root, ASnumber):
-    return root.find('./AutonomousSystems/AS[@name="' + ASnumber + '"]/protocol').text
+def getASLinksNum(root, ASnumber) : #A jour
+    i = getASPosition(root, ASnumber)
+    return int((root[i]).get('nblinks'))
 
-def getFileName(router) :
+def getASProtocol(root, ASnumber): #A jour
+    i = getASPosition(root, ASnumber)
+    return (root[i]).get('protocol')
+
+def getASAddress(root, ASnumber): #Defectueux
+    i = getASPosition(root, ASnumber)
+    return (root[i]).get('addressing')
+
+def getASName(root, AS) : #A jour
+    return int(AS.get('name'))
+
+def getFileName(router) : #A jour
     number = router.get('Rnumber')
     fileName = 'i'+number+'_startup-config.cfg'
     return fileName
@@ -31,7 +77,7 @@ def getFileName(router) :
 def getFilePath(router) :
     pass
 
-def openConfFile(router) :
+def openConfFile(router) : #A jour
     filePath = getFilePath(router)
     return open(filePath, "a")
 
@@ -39,13 +85,13 @@ def writeAddresses(router, interface, f):
 
     hostname = router.get('hostname')
 
-    f.write("interface "+interface[0].text+interface[1].text+"/0\n")
+    f.write("interface "+interface[0].text+interface.get('number')+"/0\n")
     f.write(" no ip address\n")
-    if interface[1] == 0 :
+    if int(interface.get('number')) == 0 :
         f.write(" duplex full\n")
     else :
         f.write(" negotiation auto\n")
-    f.write(" ipv6 address "+interface[2].text+"/64\n")
+    f.write(" ipv6 address "+interface[1].text+"/64\n")
     f.write(" ipv6 enable\n")
     if hostname in root[1][0] :
         f.write(" ipv6 rip ripng enable\n")
@@ -98,32 +144,109 @@ def defaultInfoFoot(f, router):
     f.write(" login\n")
     f.write("end\n")
 
-def deployProtocol(router):
-    hostname = router.get('hostname')
-    n = router.get('Rnumber')
+def getRoutersArray(root):
+    rows = int(root.get('qtty'))
+    cols = 1
+    for AS in root.iter('AS') :
+        if int(AS.get('nbrouters')) > cols :
+            cols = int(AS.get('nbrouters'))
+        
+    print(rows, cols)
+    arr = [[0 for i in range(cols)] for j in range(rows)]
+    print(arr[0] is arr[1])
 
-    ASN = 111
+    #for router in root.iter('router') :
 
-    if hostname in root[1][0] :
-        ASN = 111
-    elif hostname in root[1][1] :
-        ASN = 222
+    for i in range(len(root)) :
+        for j in range(len(root[i])) :
+            arr[i][j]=root[i][j].get('hostname')
+            print(arr[i][j])
 
-    ASN = str(ASN)
+    return arr
+
+def getASOfRouter(root, router): #A jour
+    lAS = getASList(root)
+    for AS in lAS :
+        pass
+    l1 = getRoutersInAS(root, 111)
+    l2 = getRoutersInAS(root, 222)
+    hostname = getRouterName(router)
+
+    if hostname in l1 :
+        return 111
+    if hostname in l2 :
+        return 222
+
+def getOtherASes(router):
+    pass
+
+def isBorderRouter(root, router) : #A jour
+    hostname = getRouterName(router)
+    lNeighbors = getNeighbors(router)
+    CurrentASRouters = getRoutersInAS(root, getASOfRouter(root, router))
+
+    border = False
+    Router = 'None'
+    address_ebgp = 'None'
+    i = 0
+
+    for rtr in lNeighbors :
+        if rtr not in CurrentASRouters :
+            border = True
+            Router = lNeighbors[i]
+        i+=1
+
+    address_ebgp = str(getInterfaceFromNei(router, Router).find('address').text)
+    return (border,Router,address_ebgp)
     
-    f.write("router bgp "+ ASN+"\n")
+def deployProtocol(root, router):
+    hostname = getRouterName(router)
+    n = getRouterNumber(router)
+    ASN = getASOfRouter(root, router)
+    ASNs = str(ASN)
+    border, borderNei, address_ebgp = isBorderRouter(root, router)
+
+    OASN = "222"
+
+
+    f.write("router bgp "+ ASNs+"\n")
     f.write(" bgp router-id "+n+"."+n+"."+n+"."+n+"\n")
     f.write(" bgp log-neighbor-changes\n")
     f.write(" no bgp default ipv4-unicast\n")
-    for interface in router :
-        f.write(" neighbor "+interface[2].text+" remote-as "+ASN+"\n")
+
+    if border : 
+        for interface in router :
+            if interface.find('neighbor').text != borderNei :
+                f.write(" neighbor "+interface[1].text+" remote-as "+ASNs+"\n")
+            else :
+                f.write(" neighbor "+interface[1].text+" remote-as "+ OASN+"\n")
+
+    else : 
+        for interface in router :
+            f.write(" neighbor "+interface[1].text+" remote-as "+ASNs+"\n")
+        
     f.write(" !\n")
     f.write(" address-family ipv4\n")
     f.write(" exit-address-family\n")
     f.write(" !\n")
     f.write(" address-family ipv6\n")
+
+    if border :
+        address = getASAddress(root, ASN)
+        address = address[0:11]
+        print(address)
+
+        nblinks = getASLinksNum(root, ASN)
+        for i in range(1,nblinks+1) :
+            f.write("  network "+address+str(i)+"::/64\n")
+
+        # adresse de la liaison interAS :
+        network_ebgp = address_ebgp[:-1]
+        f.write("  network "+network_ebgp+"/64\n")
+
+
     for interface in router :
-        f.write(" neighbor "+interface[2].text+" activate\n")
+        f.write("  neighbor "+interface[1].text+" activate\n")
     f.write(" exit-address-family\n")
     f.write("!\n")
 
@@ -135,30 +258,30 @@ if __name__ == "__main__":
 
     root = initXML() # crée la racine et la stocke dans la variable root
 
-    for data in root :
-        if data.tag == 'routers' : 
-            for router in data :
-                # on génere la config file et on la remplit d'abord avec ce qui ne dépend pas des interfaces
-                f = open(getFileName(router), "a")
 
-                defaultInfoHead(f, router)
+    for AS in root :
+        for router in AS :
+            # on génere la config file et on la remplit d'abord avec ce qui ne dépend pas des interfaces
+            f = open(getFileName(router), "w")
 
-                for interface in router :
-                    writeAddresses(router, interface,f)
+            defaultInfoHead(f, router)
 
-                deployProtocol(router)
+            for interface in router :
+                writeAddresses(router, interface,f)
 
-                defaultInfoFoot(f, router)
+            deployProtocol(root, router)
 
-                f.close()
+            defaultInfoFoot(f, router)
+
+            f.close()
 
     
 
     # il reste les cas suivants :
     # eBGP (remote-as)
-    # corriger l'emplacer de création des fichiers
     # faire des tests sur GNS3 et comparer les config files, encore aucun test fait à part génerer les files
-
+    # GNS3 fy
+    # spécifier le nombre d'AS au début
 
 
 
